08e3e9ea2440d0a337b4283f7d297f491ba20a39
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-03-31 14:16:46 -0400

    fix

--- ext/MPSKitPeriodicITensorExt/MPSKitPeriodicITensorExt.jl
+++ ext/MPSKitPeriodicITensorExt/MPSKitPeriodicITensorExt.jl
@@ -1,6 +1,7 @@
 module MPSKitPeriodicITensorExt
 
-using ITensorIMPS, MPSKit, TensorKit, TensorKitAdapters, PeriodicArrays
+using ITensorIMPS, MPSKit, TensorKit, TensorKitAdapters
+import PeriodicArrays: PeriodicVector
 
 include("../itensor/itensor_utility.jl")
 include("../itensor/mpskit_utility.jl")
