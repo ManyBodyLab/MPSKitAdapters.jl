85d673a4a1b327d8ae9756bc967e362ba9dd5fe0
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-03-31 16:59:21 -0400

    fix method definition

--- ext/itensor/mpskit_utility.jl
+++ ext/itensor/mpskit_utility.jl
@@ -1,5 +1,5 @@
 using ITensors, MPSKit, TensorKit, TensorKitAdapters
-import TensorKit: ⊗
+import TensorKit: ⊗, space
 import MPSKit: MPOTensor, JordanMPOTensor, MPSTensor, MPSBondTensor, InfiniteMPO, InfiniteMPOHamiltonian, InfiniteMPS
 
 using BlockTensorKit: SparseBlockTensorMap, SumSpace
