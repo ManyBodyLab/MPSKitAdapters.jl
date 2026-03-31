ad249677dc0d7b49cae834b09f85b33dc2916085
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-03-31 14:25:56 -0400

    add dependency

--- ext/MPSKitPeriodicITensorExt/MPSKitPeriodicITensorExt.jl
+++ ext/MPSKitPeriodicITensorExt/MPSKitPeriodicITensorExt.jl
@@ -2,6 +2,7 @@ module MPSKitPeriodicITensorExt
 
 using ITensorIMPS, MPSKit, TensorKit, TensorKitAdapters
 import PeriodicArrays: PeriodicVector
+using MPSKitPeriodic
 
 include("../itensor/itensor_utility.jl")
 include("../itensor/mpskit_utility.jl")
