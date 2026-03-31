969a0396e06573aa5372b1dd6cdc58fceec6ac2f
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-03-31 15:39:27 -0400

    spelling error

--- ext/MPSKitPeriodicITensorExt/infinitemps.jl
+++ ext/MPSKitPeriodicITensorExt/infinitemps.jl
@@ -5,6 +5,6 @@ function MPSKitPeriodic.InfinitePeriodicMPS(mps::ITensorIMPS.InfiniteMPS, transl
 end
 
 function MPSKitPeriodic.InfinitePeriodicMPS(mps::ITensorIMPS.InfiniteCanonicalMPS, translator=x->x[1])
-    AL = PeriodicVector(complex.(MPSKit.MPSTensor.(mps.AL.data.data)), MPSKit.translator)
+    AL = PeriodicVector(complex.(MPSKit.MPSTensor.(mps.AL.data.data)), translator)
     return MPSKitPeriodic.InfinitePeriodicMPS(AL, MPSKit.MPSBondTensor(mps.C[end]))
 end
