8d24fb22059668b3ef045c46e369c0cb5982db32
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-04-02 14:57:49 -0400

    revert breaking changes

--- ext/MPSKitPeriodicITensorExt/MPSKitPeriodicITensorExt.jl
+++ ext/MPSKitPeriodicITensorExt/MPSKitPeriodicITensorExt.jl
@@ -4,6 +4,8 @@ using ITensorIMPS, MPSKit, TensorKit, TensorKitAdapters
 import PeriodicArrays: PeriodicVector
 using MPSKitPeriodic
 
+include("../itensor/itensor_utility.jl")
+include("../itensor/mpskit_utility.jl")
 include("infinitempo.jl")
 include("infinitemps.jl")
 
